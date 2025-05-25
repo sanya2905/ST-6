d8ccbb8689f82d5571b9251c66a201878cf7b5ce
Александра Вершинина <xx.vershinina.xx@yandex.ru>
2025-05-25 20:57:08 +0300

    fix

--- src/main/java/com/mycompany/app/Program.java
+++ src/main/java/com/mycompany/app/Program.java
@@ -1,17 +1,23 @@
 package com.mycompany.app;
 
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
+
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Random;
-import java.io.FileWriter;
-import java.io.PrintWriter;
-import java.io.IOException;
 
-enum State {
-    PLAYING, OWIN, XWIN, DRAW
-};
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+enum State { PLAYING, OWIN, XWIN, DRAW };
+
 
 class Player {
     public char symbol;
@@ -23,28 +29,28 @@ class Player {
 class Game {
     public State state;
     public Player player1, player2;
-    public Player cplayer; // текущий игрок
-    public int nmove; // последний шаг сделанный действующим игроком
+    public Player cplayer;
+    public int nmove;
     public char symbol;
     public static final int INF = 100;
     public int q;
     public char[] board;
 
+
     public Game() {
-        player1 = new Player();
-        player2 = new Player();
-        player1.symbol = 'X';
-        player2.symbol = 'O';
-        state = State.PLAYING;
-        board = new char[9]; // текущая доска в игре
-        for (int i = 0; i < 9; i++)
-            board[i] = ' ';
+        player1=new Player();
+        player2=new Player();
+        player1.symbol='X';
+        player2.symbol='O';
+        state=State.PLAYING;
+        board=new char[9];
+        for(int i=0;i<9;i++)
+            board[i]=' ';
     }
 
-    // возвращаем состояние игры
-    public State checkState(char[] board) {
-        // char symbol=game.symbol;//cplayer.symbol;
-        State state = State.PLAYING;
+    public State checkState(char[] board)
+    {
+        State state=State.PLAYING;
         if ((board[0] == symbol && board[1] == symbol && board[2] == symbol) ||
                 (board[3] == symbol && board[4] == symbol && board[5] == symbol) ||
                 (board[6] == symbol && board[7] == symbol && board[8] == symbol) ||
@@ -52,14 +58,17 @@ class Game {
                 (board[1] == symbol && board[4] == symbol && board[7] == symbol) ||
                 (board[2] == symbol && board[5] == symbol && board[8] == symbol) ||
                 (board[0] == symbol && board[4] == symbol && board[8] == symbol) ||
-                (board[2] == symbol && board[4] == symbol && board[6] == symbol)) {
+                (board[2] == symbol && board[4] == symbol && board[6] == symbol))
+        {
             if (symbol == 'X')
                 state = State.XWIN;
             else if (symbol == 'O')
                 state = State.OWIN;
-        } else {
+        }
+        else {
             state = State.DRAW;
-            for (int i = 0; i < 9; i++) {
+            for (int i = 0; i < 9; i++)
+            {
                 if (board[i] == ' ') {
                     state = State.PLAYING;
                     break;
@@ -68,18 +77,17 @@ class Game {
         }
         return state;
     }
-
-    // сгенерировать возможные ходы
     void generateMoves(char[] board, ArrayList<Integer> move_list) {
         for (int i = 0; i < 9; i++)
             if (board[i] == ' ')
                 move_list.add(i);
     }
 
-    // оценка позиции
-    int evaluatePosition(char[] board, Player player) {
-        State state = checkState(board);
-        if ((state == State.XWIN || state == State.OWIN || state == State.DRAW)) {
+    int evaluatePosition(char[] board, Player player)
+    {
+        State state=checkState(board);
+        if ((state == State.XWIN || state == State.OWIN || state == State.DRAW))
+        {
             if ((state == State.XWIN && player.symbol == 'X') || (state == State.OWIN && player.symbol == 'O'))
                 return +Game.INF;
             else if ((state == State.XWIN && player.symbol == 'O') || (state == State.OWIN && player.symbol == 'X'))
@@ -90,32 +98,35 @@ class Game {
         return -1;
     }
 
-    int MiniMax(char[] board, Player player) // выбор наилучшего хода
+    int MiniMax(char[] board, Player player)
     {
         int best_val = -Game.INF, index = 0;
-        ArrayList<Integer> move_list = new ArrayList<>();
+        ArrayList<Integer> move_list=new ArrayList<>();
         int[] best_moves = new int[9];
 
         generateMoves(board, move_list);
 
-        while (move_list.size() != 0) {
+        while (move_list.size()!=0) {
             board[move_list.get(0)] = player.symbol;
             symbol = player.symbol;
 
+
             int val = MinMove(board, player);
+
 
             if (val > best_val) {
                 best_val = val;
                 index = 0;
-                best_moves[index] = move_list.get(0) + 1;
-            } else if (val == best_val)
-                best_moves[++index] = move_list.get(0) + 1;
+                best_moves[index] = move_list.get(0)+1;
+            }
+            else if (val == best_val)
+                best_moves[++index] = move_list.get(0)+1;
 
             System.out.printf("\nminimax: %3d(%1d) ", 1 + move_list.get(0), val);
             board[move_list.get(0)] = ' ';
             move_list.remove(0);
         }
-        if (index > 0) {
+        if (index > 0)  {
             Random r = new Random();
             index = r.nextInt(index);
         }
@@ -126,19 +137,19 @@ class Game {
         return best_moves[index];
     }
 
-    int MinMove(char[] board, Player player) {
+    int MinMove(char[] board, Player player)  {
 
         int pos_value = evaluatePosition(board, player);
         if (pos_value != -1)
             return pos_value;
         q++;
         int best_val = +Game.INF;
-        ArrayList<Integer> move_list = new ArrayList<>();
+        ArrayList<Integer> move_list=new ArrayList<>();
 
         generateMoves(board, move_list);
 
-        while (move_list.size() != 0) {
-            symbol = (player.symbol == 'X') ? 'O' : 'X';
+        while (move_list.size()!=0) {
+            symbol= (player.symbol == 'X') ? 'O' : 'X';
             board[move_list.get(0)] = symbol;
 
             int val = MaxMove(board, player);
@@ -158,10 +169,10 @@ class Game {
             return pos_value;
         q++;
         int best_val = -Game.INF;
-        ArrayList<Integer> move_list = new ArrayList<>();
+        ArrayList<Integer> move_list=new ArrayList<>();
         generateMoves(board, move_list);
-        while (move_list.size() != 0) {
-            symbol = (player.symbol == 'X') ? 'X' : 'O';
+        while (move_list.size()!=0) {
+            symbol=(player.symbol == 'X') ? 'X' : 'O';
             board[move_list.get(0)] = symbol;
             int val = MinMove(board, player);
             if (val > best_val) {
@@ -178,10 +189,9 @@ public class Program {
 
     public static FileWriter fileWriter;
     public static PrintWriter printWriter;
-
     public static void main(String[] args) throws IOException {
         JFrame frame = new JFrame("Demo");
-        frame.add(new TicTacToePanel(new GridLayout(3, 3)));
+        frame.add(new TicTacToePanel(new GridLayout(3,3)));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setBounds(5, 5, 500, 500);
         frame.setVisible(true);
@@ -195,148 +205,118 @@ class TicTacToeCell extends JButton {
     private int col;
     private char marker;
 
-    public TicTacToeCell(int num, int x, int y) {
-        this.num = num;
-        row = y;
-        col = x;
-        marker = ' ';
+    public TicTacToeCell(int num,int x,int y) {
+        this.num=num;
+        row=y;
+        col=x;
+        marker=' ';
         setText(Character.toString(marker));
         setFont(new Font("Arial", Font.PLAIN, 40));
     }
-
     public void setMarker(String m) {
-        marker = m.charAt(0);
+        marker=m.charAt(0);
         setText(m);
         setEnabled(false);
     }
-
     public char getMarker() {
         return marker;
     }
-
     public int getRow() {
         return row;
     }
-
     public int getCol() {
         return col;
     }
-
     public int getNum() {
         return num;
     }
-
 }
 
 class Utility {
+
     public static void print(char[] board) {
         System.out.println();
-        for (int j = 0; j < board.length; j++) {
-            System.out.print(board[j]);
-            if (j < board.length - 1) {
-                System.out.print("-");
-            }
-        }
+        for(int j=0;j<9;j++)
+            System.out.print(board[j]+"-");
         System.out.println();
     }
-
     public static void print(int[] board) {
         System.out.println();
-        for (int j = 0; j < board.length; j++) {
-            System.out.print(board[j]);
-            if (j < board.length - 1) {
-                System.out.print("-");
-            }
-        }
+        for(int j=0;j<9;j++)
+            System.out.print(board[j]+"-");
         System.out.println();
     }
-
     public static void print(ArrayList<Integer> moves) {
         System.out.println();
-        for (int j = 0; j < moves.size(); j++) {
-            System.out.print(moves.get(j));
-            if (j < moves.size() - 1) {
-                System.out.print("-");
-            }
-        }
+        for(int j=0;j<moves.size();j++)
+            System.out.print(moves.get(j)+"-");
         System.out.println();
     }
 }
 
 class TicTacToePanel extends JPanel implements ActionListener {
-
     private Game game;
 
-    private void createCell(int num, int x, int y) {
-        cells[num] = new TicTacToeCell(num, x, y);
+    private void createCell(int num,int x,int y) {
+        cells[num]=new TicTacToeCell(num,x,y);
         cells[num].addActionListener(this);
         add(cells[num]);
 
     }
 
     private TicTacToeCell[] cells = new TicTacToeCell[9];
-
     TicTacToePanel(GridLayout layout) {
         super(layout);
-        createCell(0, 0, 0);
-        createCell(1, 1, 0);
-        createCell(2, 2, 0);
-        createCell(3, 0, 1);
-        createCell(4, 1, 1);
-        createCell(5, 2, 1);
-        createCell(6, 0, 2);
-        createCell(7, 1, 2);
-        createCell(8, 2, 2);
-        game = new Game();
-        game.cplayer = game.player1;
-    }
-
-    public TicTacToeCell[] getCells() {
-        return cells;
-    }
-
-    public Game getGame() {
-        return game;
+        createCell(0,0,0);
+        createCell(1,1,0);
+        createCell(2,2,0);
+        createCell(3,0,1);
+        createCell(4,1,1);
+        createCell(5,2,1);
+        createCell(6,0,2);
+        createCell(7,1,2);
+        createCell(8,2,2);
+        game=new Game();
+        game.cplayer=game.player1;
     }
 
     public void actionPerformed(ActionEvent ae) {
         game.player1.move = -1;
         game.player2.move = -1;
-        // System.out.println(game.cplayer.symbol);
-        // System.out.println(((TicTacToeCell)(ae.getSource())).getNum());
-
-        int i = 0;
-        for (TicTacToeCell jb : cells) {
-            if (ae.getSource() == jb) {
+        int i=0;
+        for(TicTacToeCell jb: cells) {
+            if(ae.getSource()==jb) {
                 jb.setMarker(Character.toString(game.cplayer.symbol));
             }
-            game.board[i++] = jb.getMarker();
+            game.board[i++]=jb.getMarker();
         }
-        if (game.cplayer == game.player1) {
-
+        if(game.cplayer==game.player1) {
             game.player2.move = game.MiniMax(game.board, game.player2);
             game.nmove = game.player2.move;
             game.symbol = game.player2.symbol;
             game.cplayer = game.player2;
-            if (game.player2.move > 0)
-                cells[game.player2.move - 1].doClick();
-        } else {
+            if(game.player2.move>0)
+                cells[game.player2.move-1].doClick();
+        }
+        else
+        {
             game.nmove = game.player1.move;
             game.symbol = game.player1.symbol;
             game.cplayer = game.player1;
         }
+        game.state=game.checkState(game.board);
 
-        game.state = game.checkState(game.board);
-
-        if (game.state == State.XWIN) {
-            JOptionPane.showMessageDialog(null, "Выиграли крестики", "Результат", JOptionPane.WARNING_MESSAGE);
+        if(game.state==State.XWIN) {
+            JOptionPane.showMessageDialog(null,"Выиграли крестики","Результат", JOptionPane.WARNING_MESSAGE);
             System.exit(0);
 
-        } else if (game.state == State.OWIN) {
-            JOptionPane.showMessageDialog(null, "Выиграли нолики", "Результат", JOptionPane.WARNING_MESSAGE);
+        }
+        else if(game.state==State.OWIN) {
+            JOptionPane.showMessageDialog(null,"Выиграли нолики","Результат", JOptionPane.WARNING_MESSAGE);
             System.exit(0);
-        } else if (game.state == State.DRAW) {
-            JOptionPane.showMessageDialog(null, "Ничья", "Результат", JOptionPane.WARNING_MESSAGE);
+        }
+        else if(game.state==State.DRAW) {
+            JOptionPane.showMessageDialog(null,"Ничья","Результат", JOptionPane.WARNING_MESSAGE);
             System.exit(0);
         }
     }
--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -1,19 +1,224 @@
 package com.mycompany.app;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.awt.Component;
+import java.awt.GridLayout;
+import java.util.ArrayList;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest {
-
-    /**
-     * Rigorous Test :-)
-     */
+    private Game game;
+    private Player player;
+    @BeforeEach
+    public void setUp() {
+        game = new Game();
+        player = new Player();
+        player.symbol = 'X';
+    }
     @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
+    public void test1() {
+        assertEquals(State.PLAYING, game.state);
+        assertNotNull(game.board);
+        assertEquals(9, game.board.length);
+        for (char c : game.board) {
+            assertEquals(' ', c);
+        }
+    }
+
+    @Test
+    public void test2() {
+        Player p = new Player();
+        assertEquals('\u0000', p.symbol);
+        assertEquals(0, p.move);
+        assertFalse(p.selected);
+        assertFalse(p.win);
+    }
+
+    @Test
+    public void test3() {
+        char[] b = {'X',' ',' ',' ',' ',' ',' ',' ',' '};
+        game.symbol = 'X';
+        assertEquals(State.PLAYING, game.checkState(b));
+    }
+
+    @Test
+    public void test4() {
+        char[] b = {'X','O','X','X','O','O','O','X','X'};
+        game.symbol = 'X';
+        assertEquals(State.DRAW, game.checkState(b));
+        game.symbol = 'O';
+        assertEquals(State.DRAW, game.checkState(b));
+    }
+
+    @Test
+    public void test5() {
+        char[] b = {'X','X','X',' ',' ',' ',' ',' ',' '};
+        game.symbol = 'X';
+        assertEquals(State.XWIN, game.checkState(b));
+    }
+
+    @Test
+    public void test6() {
+        char[] b = {'O',' ',' ','O',' ',' ','O',' ',' '};
+        game.symbol = 'O';
+        assertEquals(State.OWIN, game.checkState(b));
+    }
+
+    @Test
+    public void test7() {
+        char[] b = {'X',' ',' ',' ','X',' ',' ',' ','X'};
+        game.symbol = 'X';
+        assertEquals(State.XWIN, game.checkState(b));
+    }
+
+    @Test
+    public void test8() {
+        char[] b = {' ',' ','O',' ','O',' ','O',' ',' '};
+        game.symbol = 'O';
+        assertEquals(State.OWIN, game.checkState(b));
+    }
+
+    @Test
+    public void test9() {
+        ArrayList<Integer> moves = new ArrayList<>();
+        game.generateMoves(game.board, moves);
+        assertEquals(9, moves.size());
+        for (int i = 0; i < 9; i++) assertTrue(moves.contains(i));
+    }
+
+    @Test
+    public void test10() {
+        char[] b = {'X','O',' ',' ','X','O','X',' ',' '};
+        ArrayList<Integer> moves = new ArrayList<>();
+        game.generateMoves(b, moves);
+        assertEquals(4, moves.size());
+        assertTrue(moves.contains(2));
+        assertTrue(moves.contains(3));
+        assertTrue(moves.contains(7));
+        assertTrue(moves.contains(8));
+    }
+
+    @Test
+    public void test11() {
+        char[] b = {'X','O','X','X','O','O','O','X','X'};
+        player.symbol = 'X';
+        assertEquals(0, game.evaluatePosition(b, player));
+    }
+
+    @Test
+    public void test12() {
+        char[] b = {'X','X','X',' ',' ',' ',' ',' ',' '};
+        game.symbol = 'X';
+        player.symbol = 'X';
+        assertEquals(Game.INF, game.evaluatePosition(b, player));
+    }
+
+    @Test
+    public void test13() {
+        char[] b = {'X','X','X',' ',' ',' ',' ',' ',' '};
+        game.symbol = 'X';
+        player.symbol = 'O';
+        assertEquals(-Game.INF, game.evaluatePosition(b, player));
+    }
+
+    @Test
+    public void test14() {
+        char[] b = {'O','O','O',' ',' ',' ',' ',' ',' '};
+        game.symbol = 'O';
+        player.symbol = 'O';
+        assertEquals(Game.INF, game.evaluatePosition(b, player));
+    }
+
+    @Test
+    public void test15() {
+        char[] b = {'O','O','O',' ',' ',' ',' ',' ',' '};
+        game.symbol = 'O';
+        player.symbol = 'X';
+        assertEquals(-Game.INF, game.evaluatePosition(b, player));
+    }
+
+    @Test
+    public void test16() {
+        char[] b = {'X','O',' ',' ',' ',' ',' ',' ',' '};
+        player.symbol = 'X';
+        int v = game.MinMove(b, player);
+        assertTrue(v >= -Game.INF && v <= Game.INF);
+    }
+
+    @Test
+    public void test17() {
+        char[] b = {'X','O',' ',' ',' ',' ',' ',' ',' '};
+        player.symbol = 'X';
+        int v = game.MaxMove(b, player);
+        assertTrue(v >= -Game.INF && v <= Game.INF);
+    }
+
+    @Test
+    public void test18() {
+        player.symbol = 'X';
+        int mv = game.MiniMax(game.board, player);
+        assertTrue(mv >= 1 && mv <= 9);
+    }
+
+    @Test
+    public void test19() {
+        char[] b = {'X','O',' ',' ',' ',' ',' ',' ',' '};
+        Utility.print(b);
+    }
+
+    @Test
+    public void test20() {
+        int[] a = {6,2,4,1,8,6,7,5,9};
+        Utility.print(a);
+    }
+
+    @Test
+    public void test21() {
+        ArrayList<Integer> L = new ArrayList<>();
+        L.add(7);
+        L.add(6);
+        Utility.print(L);
+    }
+
+    @Test
+    public void test22() {
+        TicTacToeCell c = new TicTacToeCell(8, 2, 1);
+        assertEquals(8, c.getNum());
+        assertEquals(2, c.getCol());
+        assertEquals(1, c.getRow());
+        assertEquals(' ', c.getMarker());
+    }
+
+    @Test
+    public void test23() {
+        TicTacToeCell c = new TicTacToeCell(0, 0, 0);
+        c.setMarker("O");
+        assertEquals('O', c.getMarker());
+        assertFalse(c.isEnabled());
+    }
+
+    @Test
+    public void test24() {
+        TicTacToePanel panel = new TicTacToePanel(new GridLayout(3,3));
+        assertEquals(9, panel.getComponentCount());
+        for (Component comp : panel.getComponents()) {
+            assertInstanceOf(TicTacToeCell.class, comp);
+        }
+    }
+
+    @Test
+    public void test25() {
+        System.setProperty("java.awt.headless", "true");
+        TicTacToePanel panel = new TicTacToePanel(new GridLayout(3,3));
+        TicTacToeCell cell0 = (TicTacToeCell) panel.getComponent(0);
+        cell0.doClick();
+        char m = cell0.getMarker();
+        assertTrue(m=='X' || m=='O');
     }
 }
